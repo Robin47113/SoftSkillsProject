73ba5eee0fa20537cd17366f5e50fa476199f6ce
FireMatrix <tim.schroeder02@gmx.de>
2022-07-09 15:49:51 +0200

    added weight checking

--- src/main.cpp
+++ src/main.cpp
@@ -23,6 +23,28 @@ HX711 scale2;
 #define LED_COUNT 15
 Adafruit_NeoPixel strip = Adafruit_NeoPixel(LED_COUNT, LED_PIN, NEO_GRB + NEO_KHZ800);
 
+//-------loop timing
+int time; //timestamp (in millis since chip was turned on) of the last weight measurement.
+#define WEIGHT_TAKING_DELAY 10000//after this time (in milliseconds) the weight is measured again.
+#define WEIGHT_TAKING_DELAY_FILLING 500//after this time (in milliseconds) the weight is measured again during filling.
+int fillingMode = 0;//0=normal functionality, 1=checking for Time Threshold, 2=waiting for filling to stop
+//-------weight
+#define BOTTLE_WEIGHT_EMPTY 100
+#define PEDESTAL_WEIGHT_EMPTY 100
+#define FILLING_THRESHOLD_WEIGHT 100//weight that has to be added for the bottle to accept it as being filled.
+#define FILLING_THRESHOLD_TIME 100//time that the additional weight has to be measured for to accept it as being filled.
+
+//if the bottle is in filling mode and less than this is added between two weight measurements, the filling process is
+//considered done.
+#define LOADCELL_ERROR_MARGIN 100
+
+int weight = 0;
+int weightPrev = 0;
+#define RECOMMENDED_MAX_CONSUMPTION 2000//amount of water that should be drunk every day.
+//--------liquid consumption data
+int consumptionWeek[7] = {0,0,0,0,0,0,0};//rightmost is the current day, left is 6 days ago
+int lastSessionWeight = 0;
+int lastSessionTimestamp; //TODO: good way to store the timestamp
 
 //mqtt settings
 IPAddress server(192, 168, 178, 75);//mqtt broker ip
@@ -32,11 +54,6 @@ const char* mqtt_pw = "test123";
 
 WiFiClient espClient;
 PubSubClient client(espClient);//mqtt client
-
-
-
-
-
 
 //mqtt callback
 void callback(char* topic, byte* payload, unsigned int length) {
@@ -97,6 +114,15 @@ Serial.println();
 return 1;
 }
 
+int getLiquidWeight() { //like getWeight, but only the weight of the actual liquid is returned.
+  int temp = getWeight() - BOTTLE_WEIGHT_EMPTY - PEDESTAL_WEIGHT_EMPTY;
+  if (temp > 0) {
+    return temp;
+  } else {
+    return 0;
+  }
+}
+
 void setup() {
   Serial.begin(115200);
 
@@ -123,7 +149,8 @@ void setup() {
     strip.setPixelColor(i, strip.Color(0, 0, 0));
     strip.show();
   }
-   Serial.println("LED strip Initialized");
+  Serial.println("LED strip Initialized");
+  time = millis();
 }
 
 void loop() {
@@ -131,6 +158,49 @@ void loop() {
     reconnect();
   }
   client.loop();
+
+  //weight checking
+  if (fillingMode == 1) {
+    if (millis() > time + FILLING_THRESHOLD_TIME) {
+      time = millis();
+      weight = getLiquidWeight();
+      if (weight - weightPrev > FILLING_THRESHOLD_WEIGHT) {
+        fillingMode = 2;
+      } else {
+        fillingMode = 0;
+      }
+    }
+  } else {
+    if (fillingMode == 2) {
+      if (millis() > time + WEIGHT_TAKING_DELAY_FILLING) {
+        time = millis();
+        weight = getLiquidWeight();
+        if (weight - weightPrev < LOADCELL_ERROR_MARGIN) {
+          fillingMode = 0;
+        }
+      }
+    } else {
+      time = millis();
+      if (millis() > time + WEIGHT_TAKING_DELAY){ //checks whether the weight has to be measured again.
+        weight = getLiquidWeight();
+        if (getLiquidWeight() > PEDESTAL_WEIGHT_EMPTY + LOADCELL_ERROR_MARGIN) {
+          weightPrev = weight;
+          int deltaWeight = weight - weightPrev;
+          if (abs(deltaWeight) > LOADCELL_ERROR_MARGIN) {
+            if (deltaWeight > FILLING_THRESHOLD_WEIGHT) {
+              fillingMode = 1;
+            } else {
+              lastSessionWeight = 0 - deltaWeight;
+              //TODO change timestamp; Also needs to check whether it's a new day and then shift the data array left
+              consumptionWeek[7] = consumptionWeek[7] + lastSessionWeight;
+            }
+          }
+        }
+      }
+    }
+  }
+  
+
   //getWeight();
   //delay(1000);
  /*   for(int i=0;i<strip.numPixels();i++){
